9ac29d79342f7143f4d92822e1bca6a9d28c839e
CalciumIon <1808837298@qq.com>
2024-12-27 00:49:42 +0800

    Refactor job scheduling in listen.go: create job instances and run USDT rate job immediately before scheduling

--- src/task/listen.go
+++ src/task/listen.go
@@ -4,9 +4,16 @@ import "github.com/robfig/cron/v3"
 
 func Start() {
 	c := cron.New()
-	// 汇率监听
-	c.AddJob("@every 60s", UsdtRateJob{})
-	// trc20钱包监听
-	c.AddJob("@every 5s", ListenTrc20Job{})
+
+	// Create job instances
+	usdtJob := UsdtRateJob{}
+	trc20Job := ListenTrc20Job{}
+
+	// Run USDT rate job immediately
+	usdtJob.Run()
+
+	// Schedule jobs
+	c.AddJob("@every 60s", usdtJob)
+	c.AddJob("@every 5s", trc20Job)
 	c.Start()
 }
